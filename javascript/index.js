05f7a7904c24ae7896bec850a94f6202761c2c5d
Nick Joyce <nick.joyce@beis.gov.uk>
2018-12-19 16:51:31 +0000

    Initial Commit

---
+++ javascript/index.js
@@ -0,0 +1,44 @@
+document.querySelector('#submit').addEventListener('click', (e) => {
+    // console.log(e)
+    
+    const priorityEl = document.querySelector('#priority')
+    const changeTypeEl = document.querySelector('#changeType')
+
+    const object = {
+        userName: document.querySelector('#name').value,
+        email: document.querySelector('#email').value,
+        telNumber: document.querySelector('#telNumber').value,
+        directorate: document.querySelector('#directorate').value,
+        businessRequirement: document.querySelector('#businessRequirement').value,
+        priority: priorityEl.options[priorityEl.selectedIndex].value,
+        priorityJustification: document.querySelector('#priorityJustification').value,
+        changeType: changeTypeEl.options[changeTypeEl.selectedIndex].value,
+        level: document.querySelector('#level').value,
+        code: document.querySelector('#code').value,
+        description: document.querySelector('#description').value,
+        parentLevel: document.querySelector('#parentLevel').value,
+        parentCode: document.querySelector('#parentCode').value,
+        parentDescription: document.querySelector('#parentDescription').value,
+        mentorMap: 'Mentor Map',
+        dffType: document.querySelector('#dffType').value,
+        enable: document.querySelector('#enable').value,
+        costCentreEntity: document.querySelector('#costCentreEntity').value
+    }
+    console.log(object)
+
+    const url = 'http://localhost:3000/costcentre'
+    const params = {
+        headers: {"Content-Type" : "application/json"
+        },
+        body: JSON.stringify(object),
+        method: 'POST'
+    }
+
+    console.log(params)
+    fetch(url,params)
+    .then(data=> data.json()).then(res=>console.log(res))
+})
+
+document.querySelector('#approvals').addEventListener('click', (e) => {
+    location.assign('./approvals.html')
+})
