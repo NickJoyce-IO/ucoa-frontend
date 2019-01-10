0fd5265eca0a5b969a8fae866f420efc6b174176
NickJoyce-IO <brawlyrox@gmail.com>
2019-01-10 13:42:49 +0000

    Validation comments

--- javascript/index.js
+++ javascript/index.js
@@ -48,6 +48,8 @@ document.querySelector('#submit').addEventListener('click', (e) => {
     
 })
 
+// start of the validation
+
 document.querySelector('#code').addEventListener('change', e => {
  if (e.target.value.length < 6) {
      console.log('codes must be 6 characters')
